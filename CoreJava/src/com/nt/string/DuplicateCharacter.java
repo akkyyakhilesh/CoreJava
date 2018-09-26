d4141b104a5f8426933095654bebc858cf683cda
Akhilesh <Akki@Akki>
2018-09-26 11:15:50 +0530

    saved

--- CoreJava/src/com/nt/string/DuplicateCharacter.java
+++ CoreJava/src/com/nt/string/DuplicateCharacter.java
@@ -1,9 +1,23 @@
 package com.nt.string;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class DuplicateCharacter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a String::");
+		String s=sc.nextLine();
+		char[] ch=s.toCharArray();
+		Map<Character, Integer> charMap = new HashMap<Character, Integer>();
+		for(Character c : ch) {
+			if(charMap.containsKey(c)) 
+				charMap.put(c, charMap.get(c)+1);
+			else
+				charMap.put(c,1);
+		}
 
 	}
 
